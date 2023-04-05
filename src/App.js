d9ba45a1702231f0fa3c3490a6505ed6c85c5687
Derek Estrada <derek.estrada@ehi.com>
2023-04-05 10:57:46 -0400

    Adding App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
 import Category from './pages/Category';
 import Offers from './pages/Offers';
+import Bags from './pages/Bags';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
@@ -13,6 +14,7 @@ import Listing from './pages/Listing';
 import Contact from './pages/Contact';
 import ForgotPassword from './pages/ForgotPassword';
 import AddCatch from './pages/AddCatch';
+import AddBag from './pages/AddBag';
 import 'react-toastify/dist/ReactToastify.css'
 
 
@@ -25,6 +27,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offer' element={<Offers />} />
+          <Route path='/bags' element={<Bags />} />
         <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
@@ -33,6 +36,8 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/add-catch' element={<AddCatch />} />
+          <Route path='/add-bag' element={<AddBag />} />
+
           <Route 
             path='/category/:categoryName/:listingId' 
             element={<Listing />}
